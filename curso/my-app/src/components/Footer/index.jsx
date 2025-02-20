4cecc0489a62b622ff533b1afd286c615d2ff344
lucas mendes <lucasmndes.dev@gmail.com>
2025-02-20 13:26:37 -0300

    aprendendo sobre componentes em react

---
+++ curso/my-app/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <h2> LucasFlix &copy; desenvolvido por Lucas mendes </h2>
+        </footer>
+    )
+}
+
+export default Footer;
