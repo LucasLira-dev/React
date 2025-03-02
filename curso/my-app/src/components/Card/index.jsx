4d81331cb59da012642b697b1c7989bbc507824b
lucas mendes <lucasmndes.dev@gmail.com>
2025-03-02 14:54:23 -0300

    criando rotas dinamicas

--- curso/my-app/src/components/Card/index.jsx
+++ curso/my-app/src/components/Card/index.jsx
@@ -1,14 +1,12 @@
+import { Link } from "react-router-dom";
 import "../../App.css";
 
 function Card({id}){
     return (
         <section className="flex justify-start">
-            <a
-             href={`https://www.youtube.com/watch?v=${id}`}
-             rel="noreferrer noopener" 
-             target="_blank">
+            <Link to={`/watch/${id}`}>
                 <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} alt="Capa" className="rounded-lg w-[225px]" />
-            </a>
+            </Link>
         </section>
     )
 }
--- curso/my-app/src/pages/Watch/index.jsx
+++ curso/my-app/src/pages/Watch/index.jsx
@@ -1,15 +1,28 @@
+import { useParams } from 'react-router-dom';
 import '../../App.css';
 import Container from '../../components/Container';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
+import videos from '../../json/videos.json';
+import PageNotFound from '../PageNotFound';
 
 function Watch(){
+    const params= useParams();
+    const video= videos.find((video)=>{
+        return video.id === params.id
+    }) //encontra o video pelo id
+    if(!video){
+        return <PageNotFound />
+    }
     return(
       <>
          <Header />
         <Container>
             <section className='pt-20 flex justify-center'> 
-                <iframe width="854" height="480" src="https://www.youtube.com/embed/KsFLXOTflsM?si=Z6T8ROFGC4NKjQeg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen  ></iframe> 
+                <iframe 
+                width="854" height="480" 
+                src={`https://www.youtube.com/embed/${video.id}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"
+                allowFullScreen  ></iframe> 
             </section>
         </Container>
         <Footer/>
