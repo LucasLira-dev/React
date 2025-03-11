e5f8f6aee6ccb49e95c11901dc02dd6600b206d3
lucas mendes <lucasmndes.dev@gmail.com>
2025-03-11 14:57:40 -0300

    react icons

--- treino/my-app/src/App.jsx
+++ treino/my-app/src/App.jsx
@@ -1,28 +1,15 @@
 
-import { useState } from 'react';
-import './App.css'
-import Lista from './components/Eventos/Lista'
 import AppRoutes from './Pages/routes'
-import SeuNome from './components/Eventos/SeuNome';
-import Saudacao from './components/Eventos/Saudacao';
+
 
 
 
 function App() {
 
-  const [nome, setNome] = useState('');
+   return (
  
-  return (
-    <>
-       
-       <AppRoutes/>
-       <h1> Slate Lift </h1>
-       <SeuNome setNome={setNome}/>
-      {nome}
-      <Saudacao nome={nome}/>
-    </>
-  
-  )
+    <AppRoutes/>
+   )
 }
 
 export default App
--- treino/my-app/src/components/Header/index.jsx
+++ treino/my-app/src/components/Header/index.jsx
@@ -1,12 +1,19 @@
-import Form from '../Form';
-import styles from './Header.module.css';
+import {FaFacebook, FaInstagram, FaTwitter} from 'react-icons/fa'
 import {Link} from 'react-router-dom';
+import styles from './Header.module.css';
+
 
 function Header(){
     return(
         <header className={styles.header}>
         <Link to="/form">Form</Link>
         <Link to="/sobre"> sobre </Link>
+        <ul>
+            <li>  <FaFacebook /> </li>
+            <li>  <FaInstagram /> </li>
+            <li> <FaTwitter/></li>
+        </ul>
+     
     </header>
     )
 }
